ee8efe6a3ba085d33cc67e2293cb958dc065a402
Chamijd <chamindumax28@gmail.com>
2025-05-03 17:47:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ BOT_NAME: process.env.BOT_NAME || "CHAMA-MD-V1",
 // add bot namw here for menu
 STICKER_NAME: process.env.STICKER_NAME || "CHAMA-MD-V1",
 // type sticker pack name 
-CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
 // make this true for custum emoji react    
 CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
 // chose custom react emojis by yourself 
